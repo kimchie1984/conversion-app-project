b511aac499f109005fd70eac0d5ee73cf8338c1b
kimchie1984 <128758948+kimchie1984@users.noreply.github.com>
2023-04-23 17:07:13 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ let kilogramResult = document.getElementById("kilogram-result")
 convBtn.addEventListener("click", function () {
   let baseValue = inputArea.value
 
-  lengthResult.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet`
-  volumeResult.textContent = `${baseValue} liter = ${baseValue * literToGallon} gallon`
-  kilogramResult.textContent = `${baseValue} kilogram = ${baseValue * kiloToPound} pound`
+  let metLength = inputArea.value * meterToFeet;
+  let feetLength = inputArea.value / meterToFeet;
+  lengthResult.textContent = `${baseValue} meter = ${metLength.toFixed(3)} feet | ${baseValue} feet
+ = ${feetLength.toFixed(3)} meter`
+
+  let litVolume = inputArea.value * literToGallon;
+  let galVolume = inputArea.value / literToGallon;
+  volumeResult.textContent = `${baseValue} liter = ${litVolume.toFixed(3)} gallon | ${baseValue} gallon 
+  = ${galVolume.toFixed(3)} liter`
+
+  let kiloMass = inputArea.value * kiloToPound;
+  let poundMass = inputArea.value / kiloToPound;
+  kilogramResult.textContent = `${baseValue} kilogram = ${kiloMass.toFixed(3)} pound | ${baseValue} pound 
+  = ${poundMass.toFixed(3)} kilogram`
 
 })
 
